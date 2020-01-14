e66537a538e4a7e1b38f1a2135fca5ca9b4105d7
Demotivirus <demotivirus@gmail.com>
2020-01-14 18:39:24 +0300

    Add new package OOP

---
+++ src/OOP/theory/superr/Cat.java
@@ -0,0 +1,23 @@
+package OOP.theory.superr;
+
+public class Cat extends Animal{
+    private int weight;
+    private String name;
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    public Cat(String name, int age, int weight){
+        super(name, age);
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return super.getName();
+    }
+
+    public String getMyName(){
+        return "Cat name";
+    }
+}
