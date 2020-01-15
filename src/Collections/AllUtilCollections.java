083b14fd042e4e0920cc0fd41929eea1e01ef78c
Demotivirus <demotivirus@gmail.com>
2020-01-15 13:02:13 +0300

    Add AllCollections.class & remove pictures

---
+++ src/Collections/AllUtilCollections.java
@@ -0,0 +1,37 @@
+package Collections;
+
+import java.util.*;
+
+public class AllUtilCollections {
+    public static void main(String[] args) {
+
+        //========SET=========
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+        Set<Color> enumSet = EnumSet.allOf(Color.class);
+
+        //========LIST=========
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> stack = new Stack<>();
+        List<Integer> vector = new Vector<>();
+
+        //========QUEUE=========
+        Queue<Integer> arrayQueue = new ArrayDeque<>();
+        Queue<Integer> linkedList2 = new LinkedList<>();
+        Queue<Integer> priorityQueue = new PriorityQueue<>();
+
+        //========Map=========
+        Map<Color, Color> enumMap = new EnumMap<>(Color.class);
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        Map<Integer, Integer> hasTable = new Hashtable<>();
+        Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
+        Map<Integer, Integer> treeMap = new TreeMap<>();
+        Map<Integer, Integer> weekHashMap = new WeakHashMap<>();
+    }
+}
+
+enum Color{
+    RED, YELLOW, WHITE, BLUE;
+}
