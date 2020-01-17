5c53c1622f14ebb0e2391cedb1668bea1ae5e96a
Demotivirus <demotivirus@gmail.com>
2020-01-17 23:27:46 +0300

    Add primitive types

---
+++ src/PrimitiveTypes/Example_01.java
@@ -0,0 +1,13 @@
+package PrimitiveTypes;
+
+public class Example_01 {
+        /*
+         * byte = -128 to 127
+         * short = -32768 to 32767
+         * int = -2_147_483_648 to 2_147_483_647
+         * long = -9_223_372_036_854_775_808L to 9_223_372_036_854_775_807L
+         * float = 1.4e-45f to 3.4e+38f 7 character accuracy
+         * double = 4.9e-324 to 1.7e+308 17 character accuracy
+         * char 0 to 65536
+         */
+}
