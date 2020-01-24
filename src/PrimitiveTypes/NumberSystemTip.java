017831f4fc9d0b0cf50d72afbeeb3e57fd258598
demotivirus <demotivirus@gmail.com>
2020-01-25 00:02:28 +0300

    Up PrimitiveTypes

---
+++ src/PrimitiveTypes/NumberSystemTip.java
@@ -0,0 +1,10 @@
+package PrimitiveTypes;
+
+public class NumberSystemTip {
+    public static void main(String[] args) {
+        boolean x = false;
+        boolean y =                x == true;
+
+        System.out.println(y); //false
+    }
+}
