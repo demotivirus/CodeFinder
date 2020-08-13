00d6a037780d3156c4132c2f551213ccc9d0f00c
demotivirus <demotivirus@gmail.com>
2020-08-13 07:31:18 +0300

    Add new methods

--- src/PrimitiveTypes/Practice/OperationWithPrimitives/Operations.java
+++ src/PrimitiveTypes/Practice/OperationWithPrimitives/Operations.java
@@ -1,5 +1,6 @@
 package PrimitiveTypes.Practice.OperationWithPrimitives;
 
+import javax.crypto.spec.PSource;
 import java.util.Arrays;
 
 public class Operations {
@@ -21,6 +22,10 @@ public class Operations {
         System.out.println("==================================================================");
         printAll3LengthNumbers(arr);
         printAll3LengthSameNumbers(arr);
+        System.out.println("==================================================================");
+        eratosfen(arr);
+        luckyNumbers(arr);
+        fibonacciNumbers(arr);
         System.out.println("==================================================================");
     }
 
@@ -96,11 +101,11 @@ public class Operations {
 
         int[] res = new int[count];
 
-        int point = 0;
+        int position = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 3 == 0 && arr[i] != 0) {
-                res[point] = arr[i];
-                point++;
+                res[position] = arr[i];
+                position++;
             }
         }
         System.out.println("Numbers division to 3 " + Arrays.toString(res));
@@ -115,11 +120,11 @@ public class Operations {
 
         int[] res = new int[count];
 
-        int point = 0;
+        int position = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 9 == 0 && arr[i] != 0) {
-                res[point] = arr[i];
-                point++;
+                res[position] = arr[i];
+                position++;
             }
         }
         System.out.println("Numbers division to 9 " + Arrays.toString(res));
@@ -134,11 +139,11 @@ public class Operations {
 
         int[] res = new int[count];
 
-        int point = 0;
+        int position = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 5 == 0 && arr[i] % 7 == 0 && arr[i] != 0) {
-                res[point] = arr[i];
-                point++;
+                res[position] = arr[i];
+                position++;
             }
         }
         System.out.println("Numbers division 5 and 7 " + Arrays.toString(res));
@@ -181,11 +186,11 @@ public class Operations {
 
         int[] res = new int[count];
 
-        int point = 0;
+        int position = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 100 && arr[i] < 1000) {
-                res[point] = arr[i];
-                point++;
+                res[position] = arr[i];
+                position++;
             }
         }
         System.out.println("All 3 length numbers " + Arrays.toString(res));
@@ -195,23 +200,79 @@ public class Operations {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 100 && arr[i] < 1000)
-                if (arr[i] / 100 == arr[i] / 10 % 10)
-                    if (arr[i] / 10 % 10 == arr[i] % 10)
+                if (arr[i] / 100 == arr[i] / 10 % 10) //762 = 7 == 6 ?
+                    if (arr[i] / 10 % 10 == arr[i] % 10) //773 7 == 3 ?
                         count++;
         }
 
         int[] res = new int[count];
 
-        int point = 0;
+        int position = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 100 && arr[i] < 1000)
                 if (arr[i] / 100 == arr[i] / 10 % 10)
                     if (arr[i] / 10 % 10 == arr[i] % 10){
-                        res[point] = arr[i];
-                        point++;
+                        res[position] = arr[i];
+                        position++;
                     }
-
         }
         System.out.println("All 3 length SAME numbers " + Arrays.toString(res));
+    }
+
+    public void eratosfen(int[] arr){
+        //printBubbleSortUp(arr);
+        System.out.print("Simple numbers by Eratosfen: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0 && arr[i] != 1 || arr[i] == 2)
+                if (arr[i] % 3 != 0 || arr[i] == 3)
+                    if (arr[i] % 5 != 0 || arr[i] == 5)
+                        if (arr[i] % 7 != 0 || arr[i] == 7)
+                            if (arr[i] % 11 != 0 || arr[i] == 11)
+                                if (arr[i] % 13 != 0 || arr[i] == 13)
+                                    if (arr[i] % 17 != 0 || arr[i] == 17)
+                                        if (arr[i] % 19 != 0 || arr[i] == 19)
+                                            if (arr[i] % 23 != 0 || arr[i] == 23)
+                                                if (arr[i] % 29 != 0 || arr[i] == 29)
+                                                    System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void luckyNumbers(int[] arr){
+//        System.out.print("Lucky numbers: ");
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] % 2 != 0 || arr[i] == 2)
+//                if (arr[i] % 3 != 0 || arr[i] == 3)
+//                        if (arr[i] % 7 != 0 || arr[i] == 7)
+//                            if (arr[i] % 9 != 0 || arr[i] == 9)
+//                                if (arr[i] % 13 != 0 || arr[i] == 13)
+//                                    if (arr[i] % 15 != 0 || arr[i] == 15)
+//                                        if (arr[i] % 21 != 0 || arr[i] == 21)
+//                                            if (arr[i] % 25 != 0 || arr[i] == 25)
+//                                                if (arr[i] % 31 != 0 || arr[i] == 31)
+//                                                    System.out.print(arr[i] + " ");
+//        }
+//        System.out.println();
+    }
+
+    private int fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n - 1) + fib(n - 2);
+    }
+
+    public void fibonacciNumbers(int[] arr){
+        int[] fibonacciNumbers = new int[20];
+        for (int i = 0; i < 20; i++) {
+            fibonacciNumbers[i] = fib(i);
+        }
+
+        System.out.print("Fibonacci numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < fibonacciNumbers.length; j++) {
+                if (arr[i] == fibonacciNumbers[j])
+                    System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
     }
 }
